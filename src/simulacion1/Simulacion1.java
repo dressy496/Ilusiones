3dfdd75407916641d109b8512eb8b61237c17c62
Angel <angel.e220@gmail.com>
2024-03-25 15:11:50 -0700

    Primer commit

---
+++ src/simulacion1/Simulacion1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simulacion1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author david
+ */
+public class Simulacion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //JFrame Ilusiones = new Ilusiones();
+        
+       // Ilusiones.setVisible(true);
+        
+    }
+    
+}
